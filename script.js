26ab69f795ac1b5a1b06e7a6baa5f667075a2009
Sergey <mr.alterran@mail.ru>
2019-04-07 14:12:40 +0300

    This is our first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var c = 10;
+console.log(c);
+console.log("HELLO WORLD");
+console.log("HELLO MAMA");
+
+var person = {
+    name : "Vasya Pupkin",
+    age : 23,
+    isMarried : true
+};
+console.log(typeof("43"));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var person = {
+    name : "Vasya Pupkin",
+    age : 23,
+    isMarried : true
+};
+console.log("His name is " + person.name + " he is " + person.age + " years old");
